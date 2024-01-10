5fb24efd40c2c6c28274e27757e4ea959436fa98
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-01-10 09:16:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
+
+import MovieCard from './MovieCard';
 import './App.css';
 import SearchIcon from './search.svg';
 
@@ -16,11 +18,14 @@ const movie1 = {
 }
 
 const App = ()=>{
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
     const searchMovies = async (title)=>{
         const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
 
-        console.log(data.Search);
+        setMovies(data.Search);
     }
 
     useEffect(()=>{
@@ -34,32 +39,29 @@ const App = ()=>{
             <div className='search'>
                 <input 
                     placeholder='Search for Movies'
-                    value='Superman'
-                    onChange={()=>{}}    
+                    value={searchTerm}
+                    onChange={(e)=> setSearchTerm(e.target.value)}    
                 />
 
                 <img
                     src={SearchIcon}
                     alt='search'
-                    onClick={()=>{}}
+                    onClick={()=> searchMovies(searchTerm)}
                 />
             </div>
 
-            <div className='container'>
-                <div className='movie'>
-                    <p>{movie1.Year}</p>
-                    <div>
-                        <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.Title}/>
+            {movies?.length >0
+                ? (
+                    <div className='container'>
+                        {movies.map((movie)=> (
+                            < MovieCard movie = {movie} />
+                    ))}                
+                    </div>        
+                ) : (
+                    <div className='empty'>
+                        <h2>No movies found</h2>
                     </div>
-
-                    <div>
-                        <span>{movie1.Type}</span>
-                        <h3>{movie1.Title}</h3>
-                    </div>
-                </div>
-
-                
-            </div>
+            )}
         </div>
     );
 }
